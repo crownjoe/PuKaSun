f3c6f2168eb464529c5178a3b23f51ef3b25c5d9
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-06-14 10:44:12 +0900

    Initial Commit

---
+++ capuri-sun/capuri_sunApp.swift
@@ -0,0 +1,17 @@
+//
+//  capuri_sunApp.swift
+//  capuri-sun
+//
+//  Created by 조세연 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct capuri_sunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
