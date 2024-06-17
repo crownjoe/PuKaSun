e4007f865a76634802e63d920db22d2ba6bb8185
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-06-17 23:59:53 +0900

    [FEAT/#3] suncream뷰 UI 구현

--- capuri-sun/Extension/ColorExtension.swift
+++ capuri-sun/Extension/ColorExtension.swift
@@ -5,4 +5,44 @@
 //  Created by 조세연 on 6/17/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color{
+    
+    static let backgroundBlue = Color(hex: "4095FF")
+    static let customGray = Color(hex: "D9D9D9")
+    
+    //SuncreamView
+    static let suncreamYellow = Color(hex: "FBF9BC")
+    static let suncreamFontYellow = Color(hex: "BDBB80")
+        
+    static let suncreamPink = Color(hex: "F9A2D7")
+    static let suncreamFontPink = Color(hex: "F665BF")
+    
+    static let suncreamMint = Color(hex: "7FDBE0")
+    static let suncreamFontMint = Color(hex: "44A4AA")
+    
+    static let suncreamOrange = Color(hex: "F8DFD4")
+    static let suncreamFontOrange = Color(hex: "E7A691")
+    
+    static let suncreamBlue = Color(hex: "00CCD7")
+    static let suncreamFontBlue = Color(hex: "00929A")
+    
+    static let suncreamBackBlue = Color(hex: "66A6F7")
+    
+}
+
+extension Color {
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+}
--- capuri-sun/View/Information/SuncreamVIew.swift
+++ capuri-sun/View/Information/SuncreamVIew.swift
@@ -9,7 +9,105 @@ import SwiftUI
 
 struct SuncreamVIew: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.backgroundBlue
+            
+            VStack{
+                HStack(spacing: 294){
+                    Image("img_before")
+                    
+                    Text("닫기")
+                        .font(.system(size: 20))
+                        .foregroundColor(.customGray)
+                }
+                Spacer()
+                    .frame(height: 17)
+                
+                HStack{
+                    VStack(alignment: .leading, spacing: 6){
+                        Text("어떤 자외선 차단제를\n발라야 할까요?")
+                            .fontWeight(.bold)
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                        
+                        Text("카드를 뒤집어 주세요!")
+                            .font(.system(size: 15))
+                            .foregroundColor(.customGray)
+                            .padding(.all, 7)
+                            .background(Color.suncreamBackBlue)
+                            .cornerRadius(20)
+                    }
+                    Spacer()
+                        .frame(width: 109)
+                }
+                
+                Spacer()
+                    .frame(height: 20)
+                
+                VStack(spacing: 11){
+                    HStack(spacing: 7){
+                        VStack(spacing: 11){
+                            
+                            ZStack{
+                                Image("img_suncream1")
+                                    .resizable()
+                                    .frame(width: 190, height: 190)
+                                
+                                Text("실내")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35))
+                                    .foregroundColor(.suncreamFontYellow)
+                            }
+                            
+                            ZStack{
+                                Image("img_suncream3")
+                                    .resizable()
+                                    .frame(width: 190, height: 249)
+                                Text("스포츠\n  야외 \n  활동")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35))
+                                    .foregroundColor(.suncreamFontMint)
+                            }
+                            
+                        }
+                        
+                        VStack(spacing: 11){
+                            
+                            ZStack{
+                                Image("img_suncream2")
+                                    .resizable()
+                                    .frame(width: 160, height: 278)
+                                Text("간\n단\n한\n외\n출")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35))
+                                    .foregroundColor(.suncreamFontPink)
+                                
+                            }
+                            ZStack{
+                                Image("img_suncream4")
+                                    .resizable()
+                                    .frame(width: 160, height: 160)
+                                Text("  등산\n해수욕")
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 35))
+                                    .foregroundColor(.suncreamFontOrange)
+                            }
+                        }
+                    }
+                    ZStack{
+                        Image("img_suncream5")
+                            .resizable()
+                            .frame(width: 357, height: 118)
+                        Text("야외 물놀이            ")
+                            .fontWeight(.bold)
+                            .font(.system(size: 35))
+                            .foregroundColor(.suncreamFontBlue)
+                    }
+                    
+                }
+            }
+            
+        }.ignoresSafeArea(.all)
     }
 }
 
