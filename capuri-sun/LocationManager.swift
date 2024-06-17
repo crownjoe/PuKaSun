2b7f9a3e3f73af3e00a3281c94102dcdcde0c669
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-06-17 22:44:03 +0900

    [FEAT/#3] weatherkit 수정, 위치 권한 수정

---
+++ capuri-sun/LocationManager.swift
@@ -0,0 +1,61 @@
+//
+//  LocationManager.swift
+//  capuri-sun
+//
+//  Created by 조세연 on 6/17/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+    @Published var address: String = "주소를 가져오는 중..."
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    func getCurrentLocation(completion: @escaping (CLLocation?) -> Void) {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.locationManager.stopUpdatingLocation()
+                completion(self.locationManager.location)
+            }
+        } else {
+            completion(nil)
+        }
+    }
+
+    func reverseGeocode(_ location: CLLocation, completion: @escaping (String) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion("주소를 찾을 수 없습니다.")
+                return
+            }
+            
+            if let placemark = placemarks?.first {
+                let address = "\(placemark.locality ?? ""), \(placemark.subLocality ?? "")"
+                completion(address)
+            } else {
+                completion("주소를 찾을 수 없습니다.")
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            self.location = location
+            reverseGeocode(location) { address in
+                self.address = address
+            }
+        }
+    }
+}
--- capuri-sun/View/MainView.swift
+++ capuri-sun/View/MainView.swift
@@ -6,13 +6,79 @@
 //
 
 import SwiftUI
+import CoreLocation
+import WeatherKit
 
 struct MainView: View {
+    @StateObject private var locationManager = LocationManager()
+    @State private var location: CLLocation?
+    @State private var address: String = ""
+    @State private var uvIndex: String = ""
+    @State private var condition: String = ""
+    @State private var temperature: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Text(uvIndex)
+            
+            Text(condition)
+            
+            Text(temperature)
+            
+            Text(address)
+                .onAppear {
+                    locationManager.getCurrentLocation { location in
+                        self.location = location
+                        self.address = locationManager.address
+                        if let location = location {
+                            getWeatherInfo(location)
+                        } else {
+                            print("위치 정보를 가져올 수 없습니다.")
+                        }
+                    }
+                }
+        }
+    }
+    
+    func getWeatherInfo(_ location: CLLocation) {
+        Task {
+            do {
+                let service = WeatherService()
+                let result = try await service.weather(for: location)
+                
+                self.uvIndex = "\(result.currentWeather.uvIndex.value)"
+                
+                let temperatureValue = result.currentWeather.temperature.value
+                self.temperature = "\(String(format: "%.1f", temperatureValue))°"
+                
+                self.condition = translateCondition(result.currentWeather.condition.description)
+                
+            } catch {
+                print(String(describing: error))
+            }
+        }
+    }
+    
+    func translateCondition(_ condition: String) -> String {
+        switch condition {
+        case "Partly Cloudy", "Mostly Cloudy", "Cloudy":
+            return "흐림"
+        case "Clear", "Mostly Clear":
+            return "맑음"
+        case "Foggy":
+            return "안개"
+        case "Windy":
+            return "바람"
+        case "Rain", "Heavy Rain", "Drizzle":
+            return "비"
+        case "Snow", "Heavy Snow":
+            return "눈"
+        case "Strong Storms", "Thunder Storms":
+            return "뇌우"
+        default:
+            return condition
+        }
     }
 }
 
-#Preview {
-    MainView()
-}
