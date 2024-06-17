72e5a631baca515eecb41937b4e4da847c8af644
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-06-18 02:43:18 +0900

    [FEAT/#3] suncream 뷰 UI 구현

--- capuri-sun/View/Information/SuncreamVIew.swift
+++ capuri-sun/View/Information/SuncreamVIew.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct SuncreamVIew: View {
+    
+    @State private var imageName1 = "img_suncream1"
+    @State private var text1 = "실내"
+    
+    @State private var imageName2 = "img_suncream2"
+    @State private var text2 = "간\n단\n한\n외\n출"
+    
+    @State private var imageName3 = "img_suncream3"
+    @State private var text3 = "스포츠\n  야외 \n  활동"
+    
+    @State private var imageName4 = "img_suncream4"
+    @State private var text4 = "  등산\n해수욕"
+    
+    @State private var imageName5 = "img_suncream5"
+    @State private var text5 = "야외 물놀이            "
+    
+
     var body: some View {
         ZStack{
             Color.backgroundBlue
@@ -49,24 +66,43 @@ struct SuncreamVIew: View {
                         VStack(spacing: 11){
                             
                             ZStack{
-                                Image("img_suncream1")
+                                Image(imageName1)
                                     .resizable()
                                     .frame(width: 190, height: 190)
                                 
-                                Text("실내")
+                                Text(text1)
                                     .fontWeight(.bold)
                                     .font(.system(size: 35))
                                     .foregroundColor(.suncreamFontYellow)
+                            }.onTapGesture {
+                                
+                                // TODO: 애니메이션
+                                
+                                if imageName1 == "img_suncream1" {
+                                    imageName1 = "img_aftersuncream1"
+                                    text1 = ""
+                                } else {
+                                    imageName1 = "img_suncream1"
+                                    text1 = "실내"
+                                }
                             }
                             
                             ZStack{
-                                Image("img_suncream3")
+                                Image(imageName3)
                                     .resizable()
                                     .frame(width: 190, height: 249)
-                                Text("스포츠\n  야외 \n  활동")
+                                Text(text3)
                                     .fontWeight(.bold)
                                     .font(.system(size: 35))
                                     .foregroundColor(.suncreamFontMint)
+                            }.onTapGesture {
+                                if imageName3 == "img_suncream3" {
+                                    imageName3 = "img_aftersuncream3"
+                                    text3 = ""
+                                } else {
+                                    imageName3 = "img_suncream3"
+                                    text3 = "스포츠\n  야외 \n  활동"
+                                }
                             }
                             
                         }
@@ -74,35 +110,63 @@ struct SuncreamVIew: View {
                         VStack(spacing: 11){
                             
                             ZStack{
-                                Image("img_suncream2")
+                                Image(imageName2)
                                     .resizable()
                                     .frame(width: 160, height: 278)
-                                Text("간\n단\n한\n외\n출")
+                                Text(text2)
                                     .fontWeight(.bold)
                                     .font(.system(size: 35))
                                     .foregroundColor(.suncreamFontPink)
                                 
+                            }.onTapGesture {
+                                if imageName2 == "img_suncream2" {
+                                    imageName2 = "img_aftersuncream2"
+                                    text2 = ""
+                                } else {
+                                    imageName2 = "img_suncream2"
+                                    text2 = "간\n단\n한\n외\n출"
+                                }
                             }
+                            
+                            
                             ZStack{
-                                Image("img_suncream4")
+                                Image(imageName4)
                                     .resizable()
                                     .frame(width: 160, height: 160)
-                                Text("  등산\n해수욕")
+                                Text(text4)
                                     .fontWeight(.bold)
                                     .font(.system(size: 35))
                                     .foregroundColor(.suncreamFontOrange)
+                            } .onTapGesture {
+                                if imageName4 == "img_suncream4" {
+                                    imageName4 = "img_aftersuncream4"
+                                    text4 = ""
+                                } else {
+                                    imageName4 = "img_suncream4"
+                                    text4 = "  등산\n해수욕"
+                                }
                             }
+                            
                         }
                     }
                     ZStack{
-                        Image("img_suncream5")
+                        Image(imageName5)
                             .resizable()
                             .frame(width: 357, height: 118)
-                        Text("야외 물놀이            ")
+                        Text(text5)
                             .fontWeight(.bold)
                             .font(.system(size: 35))
                             .foregroundColor(.suncreamFontBlue)
                     }
+                    .onTapGesture {
+                                if imageName5 == "img_suncream5" {
+                                    imageName5 = "img_aftersuncream5"
+                                    text5 = ""
+                                } else {
+                                    imageName5 = "img_suncream5"
+                                    text5 = "야외 물놀이            "
+                                }
+                            }
                     
                 }
             }
