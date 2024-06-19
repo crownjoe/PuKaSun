1fefbc97c6a1c9940f8216ae64e1130520e9027a
harimpudding <rimee2@gmail.com>
2024-06-20 03:57:41 +0900

    [Feat/#8/onboardingView] 글자 다음누르면 나타나게 하는 것

--- capuri-sun/View/Onboarding/OnboardingView.swift
+++ capuri-sun/View/Onboarding/OnboardingView.swift
@@ -8,20 +8,23 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var showAdditionalText = false // 상태 변수 선언
+
     var body: some View {
-        ZStack{
-            
+        ZStack {
             Color.backgroundBlue
-            VStack(alignment: .leading, spacing: 5)
-            {
+                .ignoresSafeArea(.all)
+
+            VStack(alignment: .leading, spacing: 5) {
                 Image("img_onboard")
-                
-                                Text("자외선 차단제,")
+
+                Text("자외선 차단제,")
                     .font(.system(size: 30))
                     .foregroundColor(.white)
                     .fontWeight(.bold)
                     .padding(.top, 40)
-                HStack{
+
+                HStack {
                     Text("주기적으로")
                         .font(.system(size: 30))
                         .foregroundColor(.alarmcolor3)
@@ -31,38 +34,38 @@ struct OnboardingView: View {
                         .foregroundColor(.white)
                         .fontWeight(.bold)
                 }
-                
+
                 Text("피부를 지킬 수 있어요!")
                     .font(.system(size: 30))
                     .foregroundColor(.white)
                     .fontWeight(.bold)
-                
-                
+
                 Rectangle()
                     .frame(width: 312, height: 1)
                     .padding(.top, 20)
                     .padding(.bottom, 20)
-                                .foregroundColor(.white)
-                            
-                
-                
-                Text("자외선 지수를 실시간으로 확인하고,")
-                    .font(.system(size: 17))
                     .foregroundColor(.white)
-                    .padding(.bottom, 20)
-                
-                Text("자외선 차단제를 발라야 할 때")
-                    .font(.system(size: 17))
-                    .foregroundColor(.white)
-                    .padding(.bottom, 20)
-                
-                Text("알려드려요.")
-                    .font(.system(size: 17))
-                    .foregroundColor(.white)
-                    .padding(.bottom, 155)
-                
+
+                // 이곳에 showAdditionalText가 true일 때 나타나는 텍스트를 추가합니다.
+                if showAdditionalText {
+                    Text("자외선 지수를 실시간으로 확인하고,")
+                        .font(.system(size: 17))
+                        .foregroundColor(.white)
+                        .padding(.bottom, 20)
+                    
+                    Text("자외선 차단제를 발라야 할 때")
+                        .font(.system(size: 17))
+                        .foregroundColor(.white)
+                        .padding(.bottom, 20)
+                    
+                    Text("알려드려요.")
+                        .font(.system(size: 17))
+                        .foregroundColor(.white)
+                        .padding(.bottom, 155)
+                }
+
                 Button(action: {
-                    print("Button tapped!")
+                    showAdditionalText = true // 버튼이 눌릴 때 상태 변수를 true로 설정
                 }) {
                     Text("다음")
                         .bold()
@@ -71,12 +74,10 @@ struct OnboardingView: View {
                         .frame(width: 307.96, height: 59)
                         .background(Color.white)
                         .cornerRadius(10)
-                    
                 }
-                
+                .padding(.top, showAdditionalText ? 0 : 155) // 추가 텍스트가 없을 때의 공간 유지
             }
-            
-        }.ignoresSafeArea(.all)
+        }
     }
 }
 
